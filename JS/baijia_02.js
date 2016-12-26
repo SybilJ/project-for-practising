b7814d74e83e5a38e808c3c1520c1d1c668e46d8
Sybil_JIN <349485152@qq.com>
2016-12-26 09:23:51 +0800

    Name changed

--- JS/baijia_02.js
+++ JS/baijia_02.js
@@ -19,9 +19,14 @@ var bannerData = [
 		bgImg:"./images/banner3.jpg"
 	}
 ];
+createBanner({
+	element:"banner",
+	data:bannerData
+});
 function createBanner(option){
 	var data = option.data,
 		dataLen = data.length,
+		element = document.getElementById(option.element),
 		fragment = document.createDocumentFragment(),
 		a = 0,
 		images = data.map(function(item){
@@ -50,7 +55,26 @@ function createBanner(option){
 		},4000);
 	};
 	images[a].className = "current";
-	
+	function createButton(className){
+		var i = document.createElement("i");
+		i.className = "icon " + className;
+		i.onclick = function(){
+			clearInterval(timer);
+			timer = createTimer();
+			var _a = a;
+			if(className === "previous"){
+				a = a > 0 ? a - 1 : dataLen - 1;
+			}else{
+				a = a >= dataLen - 1 ? 0 : a + 1;
+			}
+			images[_a].className = null;
+			images[a].className = "current";
+		};
+	};
+	images[a].className = "current";
+	fragment.appendChild(createButton("previous"));
+	fragment.appendChild(createButton("next"));
+	element.appendChild(fragment);
 };
 
 // var a = document.createElement("a"),
